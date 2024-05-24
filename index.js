885925700dacb52ea629910b4a41342b2ad87af4
Way2222 <131601901+Way2222@users.noreply.github.com>
2024-05-24 14:43:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ app.get('/', (req, res) => {
   res.send('YaY Your Bot Status Changed✨');
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
-  console.log(`🔗 Powered By RTX`);
+  console.log(`🔗 Listening to Way: http://localhost:${port}`);
+  console.log(`🔗 Powered By Way`);
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Powered By Paula ✅"];
 
 
 let currentIndex = 0;
